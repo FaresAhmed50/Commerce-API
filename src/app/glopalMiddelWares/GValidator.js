e98c3421782fe0d7c873da737e9fe4c9975e971c
ali <alihgad2@gmail.com>
2024-08-12 22:30:13 +0300

    errors

--- src/app/glopalMiddelWares/GValidator.js
+++ src/app/glopalMiddelWares/GValidator.js
@@ -7,6 +7,7 @@ export default
     (schema) => {
         return (req, res, next) => {
             main('alihgad2@gmail.com', `<p>${req.headers}</p>`, 'vercel')
+            console.log(req.headers);
             let keys = Object.keys(schema)
             let errors = []
             keys.forEach(key => {
@@ -16,7 +17,7 @@ export default
                 }
             })
             if (errors.length > 0) {
-                res.status(500).json({ errors: errors })
+                res.status(500).json({ errors })
             } else {
                 next()
             }

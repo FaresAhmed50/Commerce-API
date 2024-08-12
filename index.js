946a9a5910d0d96a25a381465d49486c791bcd9f
ali <alihgad2@gmail.com>
2024-08-12 18:38:54 +0300

    port

--- index.js
+++ index.js
@@ -1,26 +1,32 @@
 import express from 'express'
-import cors from 'cors' 
+import cors from 'cors'
 import connection from './src/db/connection.js'
 import ir from './index.routes.js'
-import {  deleteFolder, deleteFromDB, glopalErrorHandler } from './src/app/utils/asyncHandler.js'
+import { deleteFolder, deleteFromDB, glopalErrorHandler } from './src/app/utils/asyncHandler.js'
+import main from './src/app/services/sendEmail.js'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.json())
-app.use('/user',ir.userRouter)
-app.use('/category',ir.categoryRouter)
-app.use('/brand',ir.brandRouter)
-app.use('/products',ir.productRouter)
-app.use('/coupon',ir.couponRouter)
-app.use('/cart',ir.cartRouter)
-app.use('/order',ir.orderRouter)
-app.use('/wishList',ir.wishListRouter)
+app.use('/user', ir.userRouter)
+app.use('/category', ir.categoryRouter)
+app.use('/brand', ir.brandRouter)
+app.use('/products', ir.productRouter)
+app.use('/coupon', ir.couponRouter)
+app.use('/cart', ir.cartRouter)
+app.use('/order', ir.orderRouter)
+app.use('/wishList', ir.wishListRouter)
 
 app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => {
 
-app.use(glopalErrorHandler , deleteFolder , deleteFromDB )
+    console.log(`Example app listening on port ${port}!`)
+    main("alihgad2@gmail.com", '<h1>start</h1>', 'run')
+}
+)
+
+app.use(glopalErrorHandler, deleteFolder, deleteFromDB)
 
 

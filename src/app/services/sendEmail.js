5175de69ac3e635309e79bc8afde2625cc6ef410
FaresAhmed50 <fares.ahmed15967@aiet.edu.eg>
2025-09-06 01:35:35 +0300

    update the nodemiller config

--- src/app/services/sendEmail.js
+++ src/app/services/sendEmail.js
@@ -3,13 +3,13 @@ import { createTransport } from "nodemailer";
 Dotenv.config()
 
 const transporter = createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false, // Use `true` for port 465, `false` for all other ports
-  auth: {
-    user: process.env.NODE_MAILLER_AUTH_EMAIL,
-    pass: process.env.NODE_MAILLER_PASSWORD,
-  },
+    service: "gmail",
+    port: 465,
+    secure: true,
+    auth: {
+        user: process.env.NODE_MAILLER_AUTH_EMAIL,
+        pass: process.env.NODE_MAILLER_PASSWORD,
+    },
 });
 
 // async..await is not allowed in global scope, must use a wrapper
